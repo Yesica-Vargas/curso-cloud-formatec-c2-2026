f86e9df26b6557cee860c3586f594ac0c712a5f0
Yesica-Vargas <yesicayaninavargas02@gmail.com>
2026-07-26 19:29:43 +0000

    LAB04 - Data Sources, Locals y Nomenclatura

---
+++ terraform/iac-lab04-data-locals/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  name_prefix = "${var.project}-${var.environment}-${var.student_identity}"
+
+  bucket_name = "${local.name_prefix}-${data.aws_caller_identity.current.account_id}"
+
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Owner       = var.student_identity
+    Course      = "formatec"
+    ManagedBy   = "terraform"
+  }
+}
---
+++ terraform/iac-lab04-data-locals/main.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket" "lab" {
+  bucket = local.bucket_name
+
+  tags = local.common_tags
+}
